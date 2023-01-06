8cd4ff6be6530f6d6a605456cc6294d61e02e227
chrystalio <chrystalio.k@outlook.com>
2023-01-06 11:22:10 +0700

    Add footer

--- resources/views/qrcode.blade.php
+++ resources/views/qrcode.blade.php
@@ -7,16 +7,24 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
 </head>
 <body>
-<h3 class="text-center my-5 pt-5">Simple QR code generator</h3>
-<div class="container-fluid text-center d-flex justify-content-center my-5 py-3">
+<h3 class="text-center my-lg-5 pt-lg-5 my-4 pt-4">Simple QR code generator</h3>
+<div class="container-fluid text-center d-flex justify-content-center my-lg-5 py-lg-3">
     <div class="py-5" style="width: 18rem;">
-        <span>{!! QrCode::size(180)->generate('https://kristoff.my.id') !!}</span>
+        <form>
+            <span>{!! QrCode::size(180)->generate('hello world') !!}</span>
             <div class="form-group my-5">
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <textarea class="form-control" name="text" id="text" rows="3"></textarea>
             </div>
             <button type="submit" class="btn btn-primary">Generate</button>
         </form>
     </div>
 </div>
+<footer>
+    <div class="container-fluid text-center d-flex justify-content-center my-lg-5 py-lg-3 my-3 pt-4">
+        <div class="pt-lg-5 mt-lg-5" style="width: 18rem;">
+            <p>©<script>document.write(new Date().getFullYear())</script> - Made with Laravel By <a href="https://kristoff.my.id">Kristoff</a></p>
+        </div>
+    </div>
+</footer>
 </body>
 </html>
