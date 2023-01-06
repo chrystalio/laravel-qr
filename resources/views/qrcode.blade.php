8032180ba23e9bfd7a92739dfa926658633ab349
chrystalio <chrystalio.k@outlook.com>
2023-01-07 00:31:10 +0700

    Set margin and add container

--- resources/views/qrcode.blade.php
+++ resources/views/qrcode.blade.php
@@ -7,24 +7,39 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
 </head>
 <body>
+
+
+<div class="container my-5 py-4">
+
 <h3 class="text-center my-lg-5 pt-lg-5 my-4 pt-4">Simple QR code generator</h3>
 <div class="container-fluid text-center d-flex justify-content-center my-lg-5 py-lg-3">
     <div class="py-5" style="width: 18rem;">
-        <form>
-            <span>{!! QrCode::size(180)->generate('hello world') !!}</span>
-            <div class="form-group my-5">
-                <textarea class="form-control" name="text" id="text" rows="3"></textarea>
-            </div>
-            <button type="submit" class="btn btn-primary">Generate</button>
-        </form>
+        <span id="qrCode"></span>
+        <textarea class="form-control" name="text" id="text" rows="3"></textarea>
+        <button type="button" onclick="submitQr()" class="btn btn-primary my-3">Generate</button>
+
     </div>
+</div>
 </div>
 <footer>
     <div class="container-fluid text-center d-flex justify-content-center my-lg-5 py-lg-3 my-3 pt-4">
         <div class="pt-lg-5 mt-lg-5" style="width: 18rem;">
-            <p>©<script>document.write(new Date().getFullYear())</script> - Made with Laravel By <a href="https://kristoff.my.id">Kristoff</a></p>
+            <p>©
+                <script>document.write(new Date().getFullYear())</script>
+                - Made with Laravel By <a href="https://kristoff.my.id">Kristoff</a></p>
         </div>
     </div>
 </footer>
+
+<script>
+    function submitQr() {
+        const text = document.getElementById('text').value;
+        const qr = document.querySelector('span');
+        qr.innerHTML = "{!! QrCode::generate('hello world') !!}";
+
+        console.log(qr);
+    }
+</script>
+
 </body>
 </html>
